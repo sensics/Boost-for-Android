84541a473a8959b58e27b770bd253bc2169845a1
Ryan Pavlik <ryan@sensics.com>
2018-04-04 20:07:31 -0500

    Add support for newer API levels

--- build_tools/dev-defaults.sh
+++ build_tools/dev-defaults.sh
@@ -6,7 +6,7 @@
 # Note: levels 6 and 7 are omitted since they have the same native
 # APIs as level 5. Same for levels 10, 11 and 12
 #
-API_LEVELS="3 4 5 8 9 12 13 14 15 16 17 18 19 21"
+API_LEVELS="3 4 5 8 9 12 13 14 15 16 17 18 19 21 22 23 24 26 27"
 
 FIRST_API64_LEVEL=21
 
